515435342cc5a593b12d1dfe3df37a8da7cbe373
PiaHelenVage <piahelen93@hotmail.com>
2020-06-11 13:10:52 +0200

    created class butikk, and hello world

---
+++ GITøvelse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GITøvelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
